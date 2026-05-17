b53ec669e380a519c55ee3acc18474c40f15cbf0
shahd khairy <shahdkhairy2026@gmail.com>
2026-05-17 14:45:05 +0300

    update2

--- src/components/Ui/FormDashboard/FormDashboard.jsx
+++ src/components/Ui/FormDashboard/FormDashboard.jsx
@@ -16,7 +16,10 @@ function FormDashboard (){
   </div>
   <div className="mb-3">
     <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
+    <input type="password"
+     onChange={(e) => {
+        sethasNotification(e.target.value.trim() !== "");}}
+    className="form-control" id="exampleInputPassword1"/>
   </div>
   <div className="mb-3 form-check">
     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
